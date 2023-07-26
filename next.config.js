ff82be9e534d13f0728d9ccacf40c998206c9706
oeyoews <jyao4783@gmail.com>
2023-07-26 14:58:01 +0800

    feat: use nextra

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.jsx",
+  defaultShowCopyCode: true,
+  staticImage: true,
+  latex: true,
+});
+
+module.exports = withNextra(nextConfig);
